d05161fde6f78ee8e22c05e5b847a6ef11ae343f
Laila Case <lai_case@hotmail.com>
2023-11-07 10:54:15 +0000

    create crud express routes

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const Restaurant = require("../models/index");
 const db = require("../db/connection");
 
 //TODO: Create your GET Request Route Below:
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/restaurants", async (req, res) => {
   const restaurants = await Restaurant.findAll();
@@ -11,9 +13,37 @@ app.get("/restaurants", async (req, res) => {
 });
 
 app.get("/restaurants/:id", async (req, res) => {
-  const id = req.params;
-  const restaurantById = await Restaurant.findByPk(id.id);
+  const id = req.params.id;
+  const restaurantById = await Restaurant.findByPk(id);
   res.json(restaurantById);
 });
+
+app.post("/restaurants", async (req, res) => {
+  const body = req.body;
+  const createRestaurant = await Restaurant.create({
+    name: body.name,
+    location: body.location,
+    cuisine: body.cuisine,
+  });
+  res.json(createRestaurant)
+});
+
+app.put("/restaurants/:id", async (req, res) => {
+  const id = req.params.id;
+  const body = req.body;
+  const foundRestaurant = await Restaurant.findByPk(id);
+  const addRestaurant = await foundRestaurant.update({
+    name: body.name,
+    location: body.location,
+    cuisine: body.cuisine,
+  });
+});
+
+app.delete("/restaurants/:id", async(req, res)=>{
+  const id = req.params.id
+  const restaurantFound = await Restaurant.findByPk(id)
+  const removed = await restaurantFound.destroy()
+  res.json(removed)
+})
 
 module.exports = app;
